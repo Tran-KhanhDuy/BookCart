b57876ced0129a562be6325d78c9c4c4700e3489
Tran-KhanhDuy <qlinh8720@gmail.com>
2025-03-18 08:07:43 +0700

    abc

--- obj/Debug/net8.0/BookCart.AssemblyInfo.cs
+++ obj/Debug/net8.0/BookCart.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookCart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dbf12e6a903e4902b3fb1ee3b501af4f9144815")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4162425ce8ead556190e711b307fccff0a1404df")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookCart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookCart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
